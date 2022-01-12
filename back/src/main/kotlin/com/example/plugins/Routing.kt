cec8dfa0ca25efe85b4100b5dc4944c12e48be8a
NickKhilko <nick.khilko@atola.com>
2022-01-12 19:09:04 +0200

    gitignore

--- back/src/main/kotlin/com/example/plugins/Routing.kt
+++ back/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,7 +1,6 @@
 package com.example.plugins
 
 import files.FileFinder
-import files.KeywordInfo
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
@@ -15,12 +14,7 @@ fun Application.configureRouting() {
             call.respondText("Hello World!")
         }
         get("/ProcessDirectoryTest"){
-            val keys = emptyList<KeywordInfo>().toMutableList()
-            keys.add(KeywordInfo("Cthulhu"))
-            keys.add(KeywordInfo("truth"))
-            keys.add(KeywordInfo("death"))
-            keys.add(KeywordInfo("mad"))
-
+            val keys = listOf("Cthulhu", "truth", "death", "mad")
             FileFinder().processDirectory("C:\\Users\\Atolanin\\Desktop\\TestFileAnalyzer", keys)
         }
     }
